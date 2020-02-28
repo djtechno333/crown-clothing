859cf97bb2e9e4f3123aeaed134cdc9e14497909
Dheeraj <dheeraj@ihealthfrontier.com>
2020-02-28 17:24:54 +0530

    implemented firebase utils, included ability to store authenticated user into our firebase database.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: '1:727998710542:web:765987497a5376d5bc0d5f'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();

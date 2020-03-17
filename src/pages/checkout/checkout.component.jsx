617a5e9eefcd995423bed0efe6a4a60441e32a81
Dheeraj <dheeraj@ihealthfrontier.com>
2020-03-17 11:34:09 +0530

    added checkout component and updated cart to use selector from reselect

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import ChcekoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {
   selectCartItems,
@@ -28,7 +29,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => cartItem.name)}
+    {cartItems.map(cartItem => (
+      <ChcekoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
 
     <div className="total">Total: ${total}</div>
   </div>
